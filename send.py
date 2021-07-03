3bb25aab9fc0ff14837c6451a7e6aa6e401c4904
Austin LaShure <austinglashure@gmail.com>
2021-07-03 13:21:27 -0400

    added more quotes from seneca

--- send.py
+++ send.py
@@ -1,14 +1,27 @@
 from etext import send_sms_via_email
 import getQuote
 
-phone_number = "321-208-6947"
+class Person:
+    def __init__(self, number, prov):
+        self.number = number
+        self.prov = prov
+
+t = Person("321-208-6947", "Metro PCS")
+mo = Person("321-288-4704", "Sprint")
+balmer = Person("321-848-1625", "Verizon")
+
+people = [t, mo, balmer]
+
 obj = getQuote.Quote("quotes.txt")
-message = obj.text + '\n' + obj.author
-provider = "Metro PCS"
+message = obj.text + obj.author
+
 stmp_server = "stmp.gmail.com"
 stmp_port = 465
 
 sender_credentials = ("goodquotessender@gmail.com", "awpfprbtwsmyyxqt")
 
-send_sms_via_email(
-    phone_number, message, provider, sender_credentials)
+for person in people:
+    send_sms_via_email(
+    person.number, message, person.prov, sender_credentials)
+
+print("Done and Dunner")
